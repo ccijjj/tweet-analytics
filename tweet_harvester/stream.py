4febdf4ce88402f2b06409ca5b55691827a084f2
ccijjj <jerryxu_0909@outlook.com>
2022-05-16 04:04:50 +0800

    Updated tweet harvester

--- tweet_harvester/stream.py
+++ tweet_harvester/stream.py
@@ -9,11 +9,12 @@ import tweepy
 from tweepy import Stream
 
 RETRY_COUNT = 3
+DB_NAME = 'new_tweets'
 
 class MyStreamListener(Stream):
-    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret, **kwargs):
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret, tweet_db, **kwargs):
         super().__init__(consumer_key, consumer_secret, access_token, access_token_secret, **kwargs)
-        self.tweet_db = kwargs['tweet_db']
+        self.tweet_db = tweet_db
         self.count = 0
     
     def on_status(self, status):
@@ -39,7 +40,7 @@ class MyStreamListener(Stream):
                            "userlocation": userlocation, "lang": lang, "geo": geo,
                            "polarity": polarity, "subjectivity": subjectivity})
             self.count += 1
-            print(f"{self.count:>5d}: {text}" )
+            print(f"{self.count:>5d}: {text}")
             logging.info(f"Status {id_str} collected")
         else:
             print(f"{-1:>5} Retweet ignored")
@@ -71,12 +72,12 @@ def get_tweet_database(client, dbname='melb_tweets'):
 ################################################################################
 
 if __name__ == '__main__':
-    current_time = time.strftime("%Y/%m/%d_%H|%M|%S", time.localtime())
+    current_time = time.strftime("%Y%m%d_%H%M%S", time.localtime())
     logging.basicConfig(filename='tweet_harvester-{}.log'.format(current_time),
                         filemode='a',
                         format='[%(asctime)s] %(name)s %(levelname)s %(message)s',
                         datefmt="%Y/%m/%d %H:%M:%S",
-                        level=logging.DEBUG)
+                        level=logging.INFO)
     
     if len(sys.argv) != 8:
         logging.critical("Incorrect format for initialising the harvester")
@@ -89,18 +90,19 @@ if __name__ == '__main__':
         logging.info("Initialising harvester...")
         twt_api = get_api(consumer_key, consumer_secret, access_token, access_token_secret)
         db_client = get_couchdb_client(db_user, db_pwd, db_address)
+        tweet_db = get_tweet_database(db_client, DB_NAME)
     except:
         logging.critical("An critical error occured when initialisng harvester, exiting...")
         sys.exit(1)
 
     retry_count = RETRY_COUNT
-    tweet_db_name = 'new_tweets'
+    
 
     logging.info("Initialisation complete! Now start streaming...")
     while (retry_count):
         try:
             logging.info(f"Stream starts. {retry_count} retry/retries remaining.")
-            stream = MyStreamListener(consumer_key, consumer_secret, access_token, access_token_secret)
+            stream = MyStreamListener(consumer_key, consumer_secret, access_token, access_token_secret, tweet_db)
             stream.filter(track=["melb", 'Melb', 'MELB', 'melbourne', 'Melbourne', 'MELBOURNE'])
         except:
             logging.exception("An error during streaming")
